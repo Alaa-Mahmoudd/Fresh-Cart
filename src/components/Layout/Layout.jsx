ac61984cb3b378ec8a184fcb5510a2f9341ae362
Alaa-Mahmoudd <alaa.mahmoud17002@gmail.com>
2025-01-31 03:53:43 +0200

    Fixed Navbar import path

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import NavBar from "/src/components/NavBar/Navbar";
+import NavBar from "./NavBar/Navbar";
 import Footer from "../Footer/Footer";
 export default function Layout() {
   return (
